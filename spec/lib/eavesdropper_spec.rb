708f7111ccb715482c4c8caa6f90aa8e9434b2e1
Joel Jackson <jackson.joel@gmail.com>
2014-11-27 15:57:53 -0800

    Add better specs around the call method

--- spec/lib/eavesdropper_spec.rb
+++ spec/lib/eavesdropper_spec.rb
@@ -2,26 +2,43 @@ require './lib/eavesdropper'
 require './lib/eavesdropper/eavesdropper'
 
 describe Eavesdropper::Eavesdropper do
+  before(:each) do
+    @log_double = double("Logger")
+    allow(@log_double).to receive(:add)
+    
+    allow(Eavesdropper).to receive(:logger).and_return(@log_double)
+  end
+
   describe "#call" do
     before(:each) do
-      @log_double = double("Logger")
-      allow(@log_double).to receive(:add)
-
-      allow(Eavesdropper).to receive(:logger).and_return(@log_double)
-
-      Eavesdropper::Eavesdropper.new(object).call(method_name, *arguments)
+      @result = Eavesdropper::Eavesdropper.new(object).call(method_name, *arguments, &block)
     end
 
     let(:object){ double("Object") }
-    let(:method_name){
-      allow(object).to receive(:some_method)
-      :some_method
-    }
-    let(:arguments) {
-      ['a', 'b']
-    }
+    let(:block){ Proc.new {} }
+    let(:arguments) { }
+    
+    context "Method that returns something" do
+      let(:method_name){
+        allow(object).to receive(:some_method).and_return("Hello world")
+        :some_method
+      }
 
-    specify { expect(object).to have_received(:some_method).with('a', 'b') }
-    specify { expect(@log_double).to have_received(:add) }
+      context "With no arguments" do
+        specify { expect(object).to have_received(:some_method).with(no_args) }
+        specify { expect(@result).to eq "Hello world" }
+        specify { expect(@log_double).to have_received(:add) }
+      end
+
+      context "With two arguments" do
+        let(:arguments) {
+          ['a', 'b']
+        }
+        
+        specify { expect(object).to have_received(:some_method).with('a', 'b') }
+        specify { expect(@result).to eq "Hello world" }
+        specify { expect(@log_double).to have_received(:add) }
+      end
+    end
   end
 end
