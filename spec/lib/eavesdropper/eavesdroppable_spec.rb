ecac93ffe824414f4c204b4b97f17545de3c7e99
Joel Jackson <jackson.joel@gmail.com>
2014-12-06 23:36:50 -0800

    Spec for eavesdrop included

--- spec/lib/eavesdropper/eavesdroppable_spec.rb
+++ spec/lib/eavesdropper/eavesdroppable_spec.rb
@@ -1,5 +1,24 @@
 require './lib/eavesdropper'
 
 describe Eavesdropper::Eavesdroppable do
-  
+  describe "self.included" do
+    let(:klass){
+      Class.new do
+        include Eavesdropper::Eavesdroppable
+        eavesdrop_on :one
+        
+        def one
+        end
+      end
+    }
+
+    context "The object" do
+      subject { klass.new }
+      
+      it { should respond_to "one_with_logging" }
+      it { should respond_to "one_without_logging" }
+      it { should respond_to "one" }
+    end
+    
+  end
 end
