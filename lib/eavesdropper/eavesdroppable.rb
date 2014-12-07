97cc35dc6461addd5ea4d94f6918f0b516071d28
Joel Jackson <jackson.joel@gmail.com>
2014-12-07 09:53:40 -0800

    Optimization to not build too many listeners.

--- lib/eavesdropper/eavesdroppable.rb
+++ lib/eavesdropper/eavesdroppable.rb
@@ -2,12 +2,13 @@ module Eavesdropper
   module Eavesdroppable
     def self.included(klass)
       klass.instance_variable_set :'@eavesdrop_logged_methods', []
+      klass.instance_variable_set :'@listener', Eavesdropper::Listener.new(self)
       klass.extend ClassMethods
       
       def klass.method_added(method_name)
         if @eavesdrop_logged_methods.include?(method_name)
           @eavesdrop_logged_methods.delete method_name
-          create_logged_method self, method_name
+          create_logged_method method_name
           self.send :alias_method, method_name, Eavesdropper::Method.new(method_name).alias_with_logging
         end
       end
@@ -17,7 +18,7 @@ module Eavesdropper
       def eavesdrop_on(*methods)
         methods.each do |method_name|
           if Klass.new(self).all_methods.include?(method_name)
-            create_logged_method self, method_name
+            create_logged_method method_name
             self.send :alias_method, method_name, Eavesdropper::Method.new(method_name).alias_with_logging
           else
             @eavesdrop_logged_methods.push method_name
@@ -27,16 +28,16 @@ module Eavesdropper
 
       
       
-      def create_logged_method(klass, method_name)
+      def create_logged_method(method_name)
         method = Eavesdropper::Method.new(method_name)
 
-        klass.send :alias_method, method.alias_without_logging, method_name
+        self.send :alias_method, method.alias_without_logging, method_name
         
-        klass.send :define_method, method.alias_with_logging do |*args|
-          Eavesdropper::Listener.new(self).call(method.alias_without_logging, *args)
+        self.send :define_method, method.alias_with_logging do |*args, &block|
+          @listener.call(method.alias_without_logging, *args, &block)
         end
 
-        klass.send :private, method.alias_with_logging unless klass.instance_methods.include?(method_name)
+        self.send :private, method.alias_with_logging unless klass.instance_methods.include?(method_name)
       end
     end
   end
